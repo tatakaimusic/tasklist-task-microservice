5c3939b900f9c5d60b07013f93e8d36566285cb7
Nikita <nikitamasslennikov@mail.ru>
2023-09-25 17:31:33 +0300

    create TaskRepository

---
+++ src/main/java/com/example/tasklisttaskmicroservice/repository/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.example.tasklisttaskmicroservice.repository;
+
+import com.example.tasklisttaskmicroservice.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query(value = """
+                        SELECT * FROM tasks t 
+                        JOIN users u on u.email = t.user_email
+                        WHERE u.email = :userEmail
+            """, nativeQuery = true)
+    Optional<Task> getAllByUserEmail(String userEmail);
+}
