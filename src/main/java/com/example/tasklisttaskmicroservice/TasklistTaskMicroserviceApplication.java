d99f0028149d723436e4dee4d51235a60abde236
Nikita <nikitamasslennikov@mail.ru>
2023-09-25 17:13:00 +0300

    Initial commit

---
+++ src/main/java/com/example/tasklisttaskmicroservice/TasklistTaskMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklisttaskmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistTaskMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistTaskMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasklisttaskmicroservice/TasklistTaskMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasklisttaskmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistTaskMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
